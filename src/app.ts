a658d570f776600c574bf70a2e39111ed828dcb3
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-11-29 08:23:57 -0600

    Feat: added controller for handle errors app

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import allRoutes from './routes'
+import './module/mongoose/mongoose.config'
+import { defaultController, errorController } from './controllers/others/other.controller'
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(allRoutes)
 app.use(express.static('dist'))
+app.use(allRoutes)
+
+app.use(defaultController)
+app.use(errorController)
 
 export default app
---
+++ src/controllers/others/other.controller.ts
@@ -0,0 +1,18 @@
+import { Response, Request, ErrorRequestHandler, NextFunction } from 'express'
+
+interface ErrorController {
+
+}
+
+const defaultController = (req: Request, res: Response): void => {
+  res.status(404).send('Sorry the end-point is not available')
+}
+
+const errorController = (error: ErrorRequestHandler, req: Request, res: Response, next: NextFunction): void => {
+  res.status(500).send(error)
+}
+
+export {
+  defaultController,
+  errorController
+}
