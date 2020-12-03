f09a378fc05d82f3168f66c31fbe71a650864589
Pablo Hernandez <pablourbanohdzvizcarra@gmail.com>
2020-12-03 12:55:53 -0600

    Fix: problem with dist

--- jest.config.ts
+++ jest.config.ts
@@ -168,7 +168,7 @@ export default {
   transform: {},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
+  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$', '/dist'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
